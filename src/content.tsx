85691d3776fba87a36802355069b0ef253f99fde
winter_x64 <itswinterx64@gmail.com>
2023-02-19 01:29:41 +0530

    updated the popup template

--- src/content.tsx
+++ src/content.tsx
@@ -21,19 +21,24 @@ function IndexPopup() {
   }, [])
 
   return (
-    <div>
-      <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h1 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-          WORDSMITHY
-        </h1>
-        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-600 dark:text-white">
-          {wikiTldr && wikiTldr["title"]}
-        </h2>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
+    
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+    </a>
+    <div className="p-5">
+        <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {wikiTldr && wikiTldr["title"]}
+            </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {wikiTldr && wikiTldr["extract"]}
         </p>
-      </div>
+        
     </div>
+</div>
+
   )
 }
 
